fda5f8aaa1f44dabf6c59dc9d08018e304b92f3b
Saul Ceja <saulceja08@gmail.com>
2023-05-11 20:03:55 -0700

    Added fetch for img urls

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello to the game')
+//consoleLog api data into the website
+fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php")
+.then(response => response.json())
+.then(cardData => renderCards(cardData.data))
+
+const renderCards = (dataArray) => {
+    console.log(dataArray)
+    dataArray.forEach(cardInfo => {
+        cardInfo.card_images.forEach(imageInfo => {
+            console.log(imageInfo.image_url)
+        })
+    })
+}
