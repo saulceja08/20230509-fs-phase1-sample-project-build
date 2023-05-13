16c71c9131245582cae27401b87586bbc6990aae
Saul Ceja <saulceja08@gmail.com>
2023-05-12 23:26:10 -0700

    adding DOM events and tab navigation

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-//consoleLog api data into the website
-fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php")
-.then(response => response.json())
-.then(cardData => renderCards(cardData.data))
-
-const renderCards = (dataArray) => {
-    console.log(dataArray)
-    dataArray.forEach(cardInfo => {
-        cardInfo.card_images.forEach(imageInfo => {
-            console.log(imageInfo.image_url)
-        })
-    })
-}
+document.addEventListener('DOMContentLoaded', function() {
+    fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php")
+      .then(response => response.json())
+      .then(cardData => renderType(cardData));
+  
+    function renderCards(insertData) {
+      const cardImages = insertData.data;
+  
+      cardImages.forEach(getImage => {
+        getImage.card_images.forEach(getNewImage => console.log(getNewImage.image_url));
+      });
+    }
+  
+    function renderType(insertCardData) {
+      const cardInfo = insertCardData.data;
+  
+      cardInfo.forEach(getCardType => console.log(getCardType.type));
+    }
+  
+    var tabButtons = document.querySelectorAll('.tabContainer .buttonContainer button');
+    var tabPanels = document.querySelectorAll('.tabContainer .tabPanel');
+  
+    function showPanel(panelIndex, colorCode) {
+      tabButtons.forEach(function(node) {
+        node.style.backgroundColor = "";
+        node.style.color = "";
+      });
+      tabButtons[panelIndex].style.backgroundColor = colorCode;
+      tabButtons[panelIndex].style.color = "white";
+      tabPanels.forEach(function(node) {
+        node.style.display = "none";
+      });
+      tabPanels[panelIndex].style.display = "block";
+      tabPanels[panelIndex].style.backgroundColor = colorCode;
+    }
+  });
+  
