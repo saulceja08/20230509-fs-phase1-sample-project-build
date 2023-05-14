28ac7c15194c4db8b17b07fa4f1444013190d885
Saul Ceja <saulceja08@gmail.com>
2023-05-14 15:18:39 -0700

    Revised imgTag append to imgCOntainer

--- script.js
+++ script.js
@@ -1,37 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php")
-      .then(response => response.json())
-      .then(cardData => renderType(cardData));
+//Make sure script is running
+console.log("Running scripts")
+
+const fetchData = () => {
+  fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+  .then(response => response.json())
+  .then(dataFetched => renderImages(dataFetched))
+}
+
+function renderImages(dataFetched){
+  const imgContainer = document.getElementById('card-container')
+
+  dataFetched.data.forEach(imageArray => {
+    imageArray.card_images.forEach(imageSelector => {
+      const imgTag = document.createElement("img")
+
+      imgTag.src = imageSelector.image_url
+      imgContainer.appendChild(imgTag)
+    })
+  })
+}
   
-    function renderCards(insertData) {
-      const cardImages = insertData.data;
-  
-      cardImages.forEach(getImage => {
-        getImage.card_images.forEach(getNewImage => console.log(getNewImage.image_url));
-      });
-    }
-  
-    function renderType(insertCardData) {
-      const cardInfo = insertCardData.data;
-  
-      cardInfo.forEach(getCardType => console.log(getCardType.type));
-    }
-  
-    var tabButtons = document.querySelectorAll('.tabContainer .buttonContainer button');
-    var tabPanels = document.querySelectorAll('.tabContainer .tabPanel');
-  
-    function showPanel(panelIndex, colorCode) {
-      tabButtons.forEach(function(node) {
-        node.style.backgroundColor = "";
-        node.style.color = "";
-      });
-      tabButtons[panelIndex].style.backgroundColor = colorCode;
-      tabButtons[panelIndex].style.color = "white";
-      tabPanels.forEach(function(node) {
-        node.style.display = "none";
-      });
-      tabPanels[panelIndex].style.display = "block";
-      tabPanels[panelIndex].style.backgroundColor = colorCode;
-    }
-  });
-  
+fetchData()
