27888c0f8131dafddd307533f4c141906c664bdb
Saul Ceja <saulceja08@gmail.com>
2023-05-15 15:30:37 -0700

    Img nor rendering. Added a troubleshoot line

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 console.log("Script is Running");
-// Fetch => make an HTTP request, retrieve all data, and return as JSON
 fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
   .then(response => response.json())
   .then(info => renderCards(info.data));
 function renderCards(jsonData) {
   const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = ""; // Clear the container before rendering new cards
   jsonData.forEach(singleCardImgList => {
     const imageList = singleCardImgList.card_images;
-    const marketPrice = singleCardImgList.card_prices
+    const marketPrice = singleCardImgList.card_prices;
     imageList.forEach(displayImage => {
       const img = document.createElement("img");
       const cardMarketPrice = marketPrice.length > 0 ? marketPrice[0].cardmarket_price : "N/A";
@@ -17,42 +17,43 @@ function renderCards(jsonData) {
       displayBox.classList.add("display-box");
       displayBox.style.display = "none";
       displayBox.innerHTML = `
-        <button class="close-button">:x:</button>
         <p>Name: ${singleCardImgList.name}</p>
         <p>Type: ${singleCardImgList.type}</p>
         <p>Price: ${cardMarketPrice}</p>
       `;
       cardContainer.appendChild(displayBox);
-      img.addEventListener("click", () => {
-        if (displayBox.style.display === "none") {
-          displayBox.style.display = "block";
-        } else {
-          displayBox.style.display = "none";
-        }
+      img.addEventListener("mouseover", () => {
+        displayBox.style.display = "block";
       });
-      const closeButton = displayBox.querySelector(".close-button");
-      closeButton.addEventListener("click", () => {
+      img.addEventListener("mouseout", () => {
         displayBox.style.display = "none";
       });
     });
   });
+  // Add event listeners to the list items with the class "link"
+  const listItems = document.querySelectorAll(".link");
+  listItems.forEach((item) => {
+    item.addEventListener("click", handleListItemClick);
+  });
 }
-// Add event listener to the form
-const form = document.getElementById("search-form");
+function handleListItemClick(event) {
+  console.log("List item clicked:", event.target.textContent);
+}
+// Add form submission event listener
+const form = document.querySelector("form");
 form.addEventListener("submit", handleFormSubmit);
 function handleFormSubmit(event) {
   event.preventDefault();
-  const input = form.querySelector("input[type='text']");
-  const searchTerm = input.value;
-  // Do something with the search term (e.g., perform a search or filter the cards)
-  console.log("Search term:", searchTerm);
-  input.value = "";
-}
-// Add event listeners to the list items with the class "link"
-const listItems = document.querySelectorAll(".link");
-listItems.forEach((item) => {
-  item.addEventListener("click", handleListItemClick);
-});
-function handleListItemClick(event) {
-  console.log("List item clicked:", event.target.textContent);
+  const searchInput = document.querySelector("input[type='text']");
+  const searchKeyword = searchInput.value.toLowerCase();
+  const displayBoxes = document.querySelectorAll(".display-box");
+  displayBoxes.forEach(displayBox => {
+    const cardName = displayBox.querySelector("p:nth-child(1)").textContent.toLowerCase();
+    const cardType = displayBox.querySelector("p:nth-child(2)").textContent.toLowerCase();
+    if (cardName.includes(searchKeyword) || cardType.includes(searchKeyword)) {
+      displayBox.style.display = "block";
+    } else {
+      displayBox.style.display = "none";
+    }
+  });
 }
