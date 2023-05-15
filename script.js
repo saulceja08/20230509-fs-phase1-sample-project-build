93bd03e78a46147a72c455b07bb5d138e358870c
Saul Ceja <saulceja08@gmail.com>
2023-05-15 14:46:27 -0700

    Added new event listener to input tag

--- script.js
+++ script.js
@@ -1,53 +1,58 @@
 console.log("Script is Running");
-
-//fetch => make an http request, retrieve all data, and return as JSON
+// Fetch => make an HTTP request, retrieve all data, and return as JSON
 fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
   .then(response => response.json())
   .then(info => renderCards(info.data));
-
 function renderCards(jsonData) {
-  //access container image
   const cardContainer = document.getElementById("card-container");
-  //Iterate through each card and retrieve the image list affiliated with each card
   jsonData.forEach(singleCardImgList => {
     const imageList = singleCardImgList.card_images;
     const marketPrice = singleCardImgList.card_prices
-    //Iterate through each image and append it to the card-container
     imageList.forEach(displayImage => {
-      //create img element and set the source to the image URL
       const img = document.createElement("img");
       const cardMarketPrice = marketPrice.length > 0 ? marketPrice[0].cardmarket_price : "N/A";
       img.src = displayImage.image_url;
       cardContainer.appendChild(img);
-
-      //Create display box for card details
-      console.log(singleCardImgList)
       const displayBox = document.createElement("div");
       displayBox.classList.add("display-box");
-      displayBox.style.display = "none"; // Hide initially
+      displayBox.style.display = "none";
       displayBox.innerHTML = `
-        <button class="close-button">❌</button>
+        <button class="close-button">:x:</button>
         <p>Name: ${singleCardImgList.name}</p>
         <p>Type: ${singleCardImgList.type}</p>
         <p>Price: ${cardMarketPrice}</p>
       `;
       cardContainer.appendChild(displayBox);
-
-      //Add click event listener to each image
       img.addEventListener("click", () => {
-        // Toggle display box visibility
         if (displayBox.style.display === "none") {
           displayBox.style.display = "block";
         } else {
           displayBox.style.display = "none";
         }
       });
-
-      //Add click event listener to close button
       const closeButton = displayBox.querySelector(".close-button");
       closeButton.addEventListener("click", () => {
         displayBox.style.display = "none";
       });
     });
   });
+}
+// Add event listener to the form
+const form = document.getElementById("search-form");
+form.addEventListener("submit", handleFormSubmit);
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const input = form.querySelector("input[type='text']");
+  const searchTerm = input.value;
+  // Do something with the search term (e.g., perform a search or filter the cards)
+  console.log("Search term:", searchTerm);
+  input.value = "";
+}
+// Add event listeners to the list items with the class "link"
+const listItems = document.querySelectorAll(".link");
+listItems.forEach((item) => {
+  item.addEventListener("click", handleListItemClick);
+});
+function handleListItemClick(event) {
+  console.log("List item clicked:", event.target.textContent);
 }
