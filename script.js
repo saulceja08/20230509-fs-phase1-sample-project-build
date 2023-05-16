9b1ee3adbf3f873f60d45230b07aef53cc63f6c4
Saul Ceja <saulceja08@gmail.com>
2023-05-15 20:43:15 -0700

    added an adjusted form for feedback. Scraped from previously used code

--- script.js
+++ script.js
@@ -1,49 +1,96 @@
 console.log("Script is Running");
-//fetch => make an http request, retrieve all data, and return as JSON
+
+const cardContainer = document.getElementById("card-container");
+const allCardsLink = document.getElementById("all-cards-link");
+const monsterCardsLink = document.getElementById("monster-cards-link");
+const spellCardsLink = document.getElementById("spell-cards-link");
+const trapCardsLink = document.getElementById("trap-cards-link");
+const feedbackLink = document.getElementById("feedback-link");
+const feedbackForm = document.getElementById("feedback-form");
+
+allCardsLink.addEventListener("click", () => filterCards("All Cards"));
+monsterCardsLink.addEventListener("click", () => filterCards("Monster Cards"));
+spellCardsLink.addEventListener("click", () => filterCards("Spell Cards"));
+trapCardsLink.addEventListener("click", () => filterCards("Trap Cards"));
+feedbackLink.addEventListener("click", () => openFeedbackForm());
+
 fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
   .then(response => response.json())
   .then(info => renderCards(info.data));
+
 function renderCards(jsonData) {
-  //access container image
-  const cardContainer = document.getElementById("card-container");
-  //Iterate through each card and retrieve the image list affiliated with each card
+  cardContainer.innerHTML = ""; // Clear previous cards
+
   jsonData.forEach(singleCardImgList => {
     const imageList = singleCardImgList.card_images;
-    const marketPrice = singleCardImgList.card_prices
-    //Iterate through each image and append it to the card-container
+    const marketPrice = singleCardImgList.card_prices;
+
     imageList.forEach(displayImage => {
-      //create img element and set the source to the image URL
       const img = document.createElement("img");
       const cardMarketPrice = marketPrice.length > 0 ? marketPrice[0].cardmarket_price : "N/A";
       img.src = displayImage.image_url;
       cardContainer.appendChild(img);
-      //Create display box for card details
-      console.log(singleCardImgList)
+
       const displayBox = document.createElement("div");
       displayBox.classList.add("display-box");
-      displayBox.style.display = "none"; // Hide initially
       displayBox.innerHTML = `
-        <button class="close-button">x</button>
-        <button class="close-button">❌</button>
         <p>Name: ${singleCardImgList.name}</p>
         <p>Type: ${singleCardImgList.type}</p>
         <p>Price: ${cardMarketPrice}</p>
       `;
       cardContainer.appendChild(displayBox);
-      //Add click event listener to each image
-      img.addEventListener("click", () => {
-        // Toggle display box visibility
-        if (displayBox.style.display === "none") {
-          displayBox.style.display = "block";
-        } else {
-          displayBox.style.display = "none";
-        }
+
+      img.addEventListener("mouseenter", () => {
+        displayBox.style.display = "block";
       });
-      //Add click event listener to close button
-      const closeButton = displayBox.querySelector(".close-button");
-      closeButton.addEventListener("click", () => {
+
+      img.addEventListener("mouseleave", () => {
         displayBox.style.display = "none";
       });
     });
   });
 }
+
+function filterCards(cardType) {
+  const allCards = Array.from(cardContainer.children);
+  
+  allCards.forEach(card => {
+    if (cardType === "All Cards") {
+      card.style.display = "block";
+    } else {
+      const cardTypeText = card.querySelector("p:nth-child(2)").textContent;
+      if (cardTypeText === cardType) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    }
+  });
+}
+
+function openFeedbackForm() {
+  feedbackForm.style.display = "block";
+}
+
+// Submit feedback form
+feedbackForm.addEventListener("submit", event => {
+  event.preventDefault();
+  
+  const nameInput = document.getElementById("name-input");
+  const emailInput = document.getElementById("email-input");
+  const commentsInput = document.getElementById("comments-input");
+  
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const comments = commentsInput.value;
+  
+  // Perform your desired actions with the form data (e.g., send it to a server)
+  
+  // Clear input fields
+  nameInput.value = "";
+  emailInput.value = "";
+  commentsInput.value = "";
+  
+  // Hide feedback form
+  feedbackForm.style.display = "none";
+});
