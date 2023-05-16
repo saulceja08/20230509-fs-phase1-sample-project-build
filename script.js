66f122891cec58e688b573a856b35e42dd66b495
Saul Ceja <saulceja08@gmail.com>
2023-05-16 11:06:57 -0700

    changed card-container to const, moved fetch request and card render into a seperate function, added comments

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 console.log("Script is Running");
-
-let cardContainer; // Define the cardContainer variable
-
+// Define the cardContainer variable
+const cardContainer = document.getElementById("card-container");
 document.addEventListener("DOMContentLoaded", () => {
-  cardContainer = document.getElementById("card-container"); // Assign the element inside the event listener
+  // Fetch card data and render cards
+  fetchCardData();
+});
+function fetchCardData() {
   const linkItems = document.querySelectorAll(".link");
-
   fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
     .then(response => response.json())
     .then(info => {
       const jsonData = info.data;
       renderCards(jsonData);
-
       // Add click event listeners to link items
       linkItems.forEach(item => {
         item.addEventListener("click", () => {
@@ -20,21 +20,17 @@ document.addEventListener("DOMContentLoaded", () => {
         });
       });
     });
-});
-
+}
 function renderCards(jsonData) {
   cardContainer.innerHTML = ""; // Clear the existing card container
-
   jsonData.forEach(singleCardImgList => {
     const imageList = singleCardImgList.card_images;
     const marketPrice = singleCardImgList.card_prices;
-
     imageList.forEach(displayImage => {
       const img = document.createElement("img");
       const cardMarketPrice = marketPrice.length > 0 ? marketPrice[0].cardmarket_price : "N/A";
       img.src = displayImage.image_url;
       cardContainer.appendChild(img);
-
       const displayBox = document.createElement("div");
       displayBox.classList.add("display-box");
       displayBox.innerHTML = `
@@ -43,21 +39,17 @@ function renderCards(jsonData) {
         <p>Price: ${cardMarketPrice}</p>
       `;
       cardContainer.appendChild(displayBox);
-
       img.addEventListener("mouseenter", () => {
         displayBox.style.display = "block";
       });
-
       img.addEventListener("mouseleave", () => {
         displayBox.style.display = "none";
       });
     });
   });
 }
-
 function filterCardsByCategory(jsonData, category) {
   cardContainer.innerHTML = ""; // Clear the existing card container
-
   if (category === "All Cards") {
     renderCards(jsonData);
   } else {
@@ -65,19 +57,14 @@ function filterCardsByCategory(jsonData, category) {
     renderCards(filteredData);
   }
 }
-
 function showPopup() {
-  //create a pop-up element when user clicks submit
-  const popup = document.createElement("div")
-  popup.className = "popup"
-  popup.innerHTML = "Thank You! Your feedback was submitted!"
-
+  // Create a pop-up element when the user clicks submit
+  const popup = document.createElement("div");
+  popup.className = "popup";
+  popup.innerHTML = "Thank You! Your feedback was submitted!";
   document.body.appendChild(popup);
-
-  setTimeout(function() {
-    popup.remove()
-
-  },2000)
-
-  document.getElementById("contact-form").reset()
+  setTimeout(() => {
+    popup.remove();
+  }, 2000);
+  document.getElementById("contact-form").reset();
 }
