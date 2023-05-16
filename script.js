2fb711c1e29182a02600429376891e48a286a57e
Saul Ceja <saulceja08@gmail.com>
2023-05-15 20:58:08 -0700

    added a new header background

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
 console.log("Script is Running");
 
-const cardContainer = document.getElementById("card-container");
-const allCardsLink = document.getElementById("all-cards-link");
-const monsterCardsLink = document.getElementById("monster-cards-link");
-const spellCardsLink = document.getElementById("spell-cards-link");
-const trapCardsLink = document.getElementById("trap-cards-link");
-const feedbackLink = document.getElementById("feedback-link");
-const feedbackForm = document.getElementById("feedback-form");
-
-allCardsLink.addEventListener("click", () => filterCards("All Cards"));
-monsterCardsLink.addEventListener("click", () => filterCards("Monster Cards"));
-spellCardsLink.addEventListener("click", () => filterCards("Spell Cards"));
-trapCardsLink.addEventListener("click", () => filterCards("Trap Cards"));
-feedbackLink.addEventListener("click", () => openFeedbackForm());
-
 fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
   .then(response => response.json())
   .then(info => renderCards(info.data));
 
 function renderCards(jsonData) {
-  cardContainer.innerHTML = ""; // Clear previous cards
+  const cardContainer = document.getElementById("card-container");
 
   jsonData.forEach(singleCardImgList => {
     const imageList = singleCardImgList.card_images;
@@ -50,47 +36,3 @@ function renderCards(jsonData) {
     });
   });
 }
-
-function filterCards(cardType) {
-  const allCards = Array.from(cardContainer.children);
-  
-  allCards.forEach(card => {
-    if (cardType === "All Cards") {
-      card.style.display = "block";
-    } else {
-      const cardTypeText = card.querySelector("p:nth-child(2)").textContent;
-      if (cardTypeText === cardType) {
-        card.style.display = "block";
-      } else {
-        card.style.display = "none";
-      }
-    }
-  });
-}
-
-function openFeedbackForm() {
-  feedbackForm.style.display = "block";
-}
-
-// Submit feedback form
-feedbackForm.addEventListener("submit", event => {
-  event.preventDefault();
-  
-  const nameInput = document.getElementById("name-input");
-  const emailInput = document.getElementById("email-input");
-  const commentsInput = document.getElementById("comments-input");
-  
-  const name = nameInput.value;
-  const email = emailInput.value;
-  const comments = commentsInput.value;
-  
-  // Perform your desired actions with the form data (e.g., send it to a server)
-  
-  // Clear input fields
-  nameInput.value = "";
-  emailInput.value = "";
-  commentsInput.value = "";
-  
-  // Hide feedback form
-  feedbackForm.style.display = "none";
-});
